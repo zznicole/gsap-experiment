82a1e0ea050f0b2f8b8babe02b4cc67bb59b61cc
zznicole <zznicole@hotmail.com>
2020-08-11 02:34:45 +0200

    done timeline

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ tl.to(".rubberband", {
     backgroundColor: "#EDC02C",
     stagger: 0.2,
 })
+  .to(".topS,.topU,.topM1,.topM2,.topE,.topR", {
+    duration: 1, 
+    stagger: 0.2,
+    opacity: 0.6,
+}, "-=.1")
 
+  .to(".topS,.topU,.topM1,.topM2,.topE,.topR", {
+    Y: 300,
+    duration: 1, 
+    stagger: 0.2,
+    opacity: 0,
+    
+})
 //   .to(".glasswater", {
 //     duration: 2,
 //     y: -20,
@@ -13,37 +25,37 @@ tl.to(".rubberband", {
 //     ease: "bounce",
 // })
   
-  .from(".waterdrop1", {
-    duration: 4, 
-    x: 150,
-    y: -150,
-    rotation: 90,
+//   .from(".waterdrop1", {
+//     duration: 4, 
+//     x: 150,
+//     y: -150,
+//     rotation: 90,
     
-})
-  .to(".waterdrop1", {
-    duration: 4, 
-    x: 150,
-    y: -150,
-    rotation: 90,
-    opacity:0,
-})
+// })
+//   .to(".waterdrop1", {
+//     duration: 4, 
+//     x: 150,
+//     y: -150,
+//     rotation: 90,
+//     opacity:0,
+// })
 
-  .to(".waterdrop2", {
-    duration: 3, 
-    x: -150,
-    y: -150,
-    rotation: -90,
-    opacity:0,
-})
+//   .to(".waterdrop2", {
+//     duration: 3, 
+//     x: -150,
+//     y: -150,
+//     rotation: -90,
+//     opacity:0,
+// })
 
-  .to(".waterdrop3", {
-    duration: 3, 
-    y: 300,
-    rotation: 360,
-    opacity:0,
-})
+//   .to(".waterdrop3", {
+//     duration: 3, 
+//     y: 300,
+//     rotation: 360,
+//     opacity:0,
+// })
   
-  .to(".watersplash,.topU,.topO,.topN", {
+  .to(".watersplash,.topO,.topN", {
     duration: 2,
     opacity: 0.5,
 }, "-=.5")
